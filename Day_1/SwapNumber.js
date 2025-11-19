d60a0e8bbac676ce7bedb2a5c8e66905a5ac0c8e
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-11-19 22:00:44 +0530

    practice

---
+++ Day_1/SwapNumber.js
@@ -0,0 +1,23 @@
+// method_1
+// let a =10;
+// let b=20;
+// let c;
+// c=a
+// a=b
+// b=c
+// console.log( 'a=',a  ,'b=',b);
+
+// method_2
+// let a =10;
+// let b=20;
+
+// a=a+b
+// b=a-b
+// a=a-b
+// console.log( 'a=',a  ,'b=',b);
+
+// method_3
+let a =120;
+let b=20;
+[a,b]=[b,a]
+console.log(a,  b);
