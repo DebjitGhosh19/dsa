206d9c662d00654df790abfdb07921590ea365e2
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2026-05-27 12:19:27 +0530

    practice

---
+++ Day_1/SwapNumbers2.js
@@ -0,0 +1,27 @@
+//Swap with extra variable
+let a=100;
+let b=20;
+
+// let c;
+
+// c=a;
+// a=b;
+// b=c;
+// console.log(a);
+// console.log(b);
+
+// 1st method Without extra variable
+
+// [a,b]=[b,a]
+// console.log(a);
+// console.log(b);
+
+// 2nd method Without extra variable
+
+a=a+b
+b=a-b
+a=a-b
+console.log(a);
+console.log(b);
+
+
