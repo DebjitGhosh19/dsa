68aeb0f05f27942f0f757e7ecedc0978a8e85fd0
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2026-06-11 21:55:12 +0530

    Day_3

---
+++ Day_3/q1/index.js
@@ -0,0 +1,10 @@
+// Sum of n natural number
+
+let n=Number(prompt("Enter your number"));
+   let sum=0;
+for(let i=1;i<=n;i++){
+ 
+    sum+=i
+    
+}
+console.log(sum);
---
+++ Day_3/q2/index.js
@@ -0,0 +1,10 @@
+// Factorial 
+
+let n=Number(prompt("Enter your number"));
+   let fact=1;
+for(let i=1;i<=n;i++){
+ 
+    fact*=i
+    
+}
+console.log(fact);
