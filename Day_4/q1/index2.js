fdffb9362e606e928d20072f945fbb9378c61d55
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2026-06-20 23:48:33 +0530

    ok

---
+++ Day_4/q1/index2.js
@@ -0,0 +1,8 @@
+for (let index = 0; index < 5; index++) {
+    
+    for (let j = 0; j < 5; j++) {
+        process.stdout.write("* ")
+    }
+  console.log();
+  
+}
---
+++ Day_4/q1/index5.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 5; i++) {
+  let asci=65
+    for (let j = 5; j >=i; j--) {
+      process.stdout.write(String.fromCharCode(asci)+" ")
+      asci++;
+    }
+console.log();
+}
