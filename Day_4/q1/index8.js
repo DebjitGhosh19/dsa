4b83e38f6178eb6070c4dfa2df9b2b0cd3e255ca
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2026-06-25 22:38:23 +0530

    Day_4 complete

--- Day_4/q1/index8.js
+++ Day_4/q1/index8.js
@@ -1,15 +1,12 @@
 let n=5
 for (let i = 1; i <=n; i++) {
- for (let j = 1; j <= n; j++) {
-   if (i==j||i+j==n+1) {
+ for (let j = 1; j <= 2*n-1; j++) {
+   if (i==j||i+j==n*2) {
     process.stdout.write("*")
    }
    else{
     process.stdout.write(" ")
    }
-    
  }
  console.log();
- 
-    
 }
